6bc93e6066cc600709c99562fa79548543f47d78
Zae <zaevi@live.com>
2018-11-29 23:59:06 +0800

    User序列化

---
+++ GMgardSigner/UserSerializer.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+
+namespace GMgardSigner
+{
+    public static class UserSerializer
+    {
+        public static byte[] ToBytes(this User user)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var bf = new BinaryFormatter();
+                bf.Serialize(ms, user.Username);
+                bf.Serialize(ms, user.Password);
+                bf.Serialize(ms, user.Cookies);
+                var bytes = ProtectedData.Protect(ms.ToArray(), null, DataProtectionScope.CurrentUser);
+                return bytes;
+            }
+        }
+
+        public static User ToUser(this byte[] bytes)
+        {
+            bytes = ProtectedData.Unprotect(bytes, null, DataProtectionScope.CurrentUser);
+            using (var ms = new MemoryStream(bytes))
+            {
+                var bf = new BinaryFormatter();
+                var username = bf.Deserialize(ms).ToString();
+                var password = bf.Deserialize(ms).ToString();
+                var cookies = bf.Deserialize(ms) as System.Net.CookieContainer;
+                var user = User.Create(username, password);
+                user.Cookies = cookies;
+                return user;
+            }
+        }
+    }
+}
